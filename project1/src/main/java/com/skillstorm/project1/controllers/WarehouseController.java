1fc9ed58a850f00f3464f1f467c90bc8f7392e8b
Code Classroom <benjamin.underdahl@proton.me>
2025-12-02 16:47:05 -0600

    added post route to add warehouse to db

--- project1/src/main/java/com/skillstorm/project1/controllers/WarehouseController.java
+++ project1/src/main/java/com/skillstorm/project1/controllers/WarehouseController.java
@@ -1,14 +1,31 @@
 package com.skillstorm.project1.controllers;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.skillstorm.project1.models.WarehouseModel;
+import com.skillstorm.project1.services.WarehouseService;
 
 @RestController
 @RequestMapping("/api")
 public class WarehouseController {
 
+    private final WarehouseService service;
+
+    public WarehouseController(WarehouseService service) {
+        this.service = service;
+    }
+
     @GetMapping("/warehouse")
     String home() {
         return "Hello World";
+    }
+
+    @PostMapping("/warehouse")
+    public WarehouseModel create(@RequestBody WarehouseModel warehouse) {
+        return service.createWarehouse(warehouse);
     }
 }
--- project1/src/main/java/com/skillstorm/project1/services/WarehouseService.java
+++ project1/src/main/java/com/skillstorm/project1/services/WarehouseService.java
@@ -1,7 +1,28 @@
 package com.skillstorm.project1.services;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.skillstorm.project1.models.WarehouseModel;
+import com.skillstorm.project1.repositories.WarehouseRepository;
 
 @Service
 public class WarehouseService {
+    
+    private final WarehouseRepository repo;
 
+    public WarehouseService(WarehouseRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<WarehouseModel> getAllWarehouses() {
+        return repo.findAll();
+    }
+
+    public WarehouseModel createWarehouse(WarehouseModel warehouse) {
+        if (warehouse.getMaxCapacity() <= 0) {
+            throw new IllegalArgumentException("Max capacity must be >= 0");
+        }
+        return repo.save(warehouse);
+    }
 }
