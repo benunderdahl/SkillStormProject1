fd3f97af2f7ee5b4ba5b680ec12c3471590174aa
Code Classroom <benjamin.underdahl@proton.me>
2025-12-05 16:14:57 -0600

    added frontend files

--- project1/backend/src/main/java/com/skillstorm/project1/controllers/WarehouseController.java
+++ project1/backend/src/main/java/com/skillstorm/project1/controllers/WarehouseController.java
@@ -1,5 +1,4 @@
 package com.skillstorm.project1.controllers;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
