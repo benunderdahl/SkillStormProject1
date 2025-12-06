4d995a2c519229d8ba8927b06ce5d58326441134
Code Classroom <benjamin.underdahl@proton.me>
2025-12-05 19:50:16 -0600

    restructured folders

--- project1/frontend/src/App.jsx
+++ project1/frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState, react } from 'react'
+import 'react-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Navbar />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ project1/frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import 'react-dom';
+
+function Navbar() {
+    return <h1>Hello World</h1>
+}
+
+export default Navbar
